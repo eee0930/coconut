b974f803b5eb61f5e8e125c577c05b347baa4eb7
lazyhysong <eee0930@naver.com>
2023-06-10 19:36:58 +0900

    feat: youtube 연결

--- src/apis/youtubeApi.ts
+++ src/apis/youtubeApi.ts
@@ -1,7 +1,34 @@
-
 const API_ROOT = process.env.REACT_APP_YOUTUBE_ROOT;
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
-export const fetchVideosByQuery = (q: string) => {
+export interface IVideoIdItem {
+  kind: string;
+  id: {
+    kind: string;
+    videoId: string;
+  };
+}
 
+export interface IVideoIdResults {
+  kind: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: IVideoIdItem[];
+}
+
+
+const fetchResponseData = (url: string) => {
+  try {
+    return fetch(url).then(response => response.json());
+  } catch (error: any) {
+    console.error("🛑", error.message);
+    throw error;
+  }
+};
+
+export const fetchVideoIdsByQuery = (q: string) => {
+  const url = `${API_ROOT}search?key=${API_KEY}&type=video&q=${q}`;
+  return fetchResponseData(url);
 }
--- src/routes/ViewMusic.tsx
+++ src/routes/ViewMusic.tsx
@@ -1,8 +1,34 @@
 import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
 
-function VeiwMusic() {
+// apis
+import { ITrackInfo, fetchTrackInfoById } from "../apis/deezerMusicApi";
+
+// styles
+import { Loader } from "../utils/globalStyles";
+import Video from "../components/Video";
+
+function ViewMusic() {
+  const { id } = useParams();
+  const { data: musicInfo, isLoading } = useQuery<ITrackInfo>(
+    "musicInfo",
+    () => fetchTrackInfoById(Number(id)),
+    { retry: 0 }
+  );
+
   useEffect(() => window.scrollTo(0, 0), []);
-  return null;
+
+  return <>
+  {isLoading ? 
+    <Loader>
+      <div>
+        <div></div><div></div>
+      </div>
+    </Loader> : <>
+    <Video query={`${musicInfo?.title_short}+${musicInfo?.artist.name}`} />
+  </>}
+  </>;
 }
 
-export default VeiwMusic;
+export default ViewMusic;
